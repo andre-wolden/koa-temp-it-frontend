c7081980097191bbbbfc89010acd472869198582
André Wolden <andre.wolden@bekk.no>
2018-04-05 09:03:39 +0200

    asdf

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-const path = require('path');
 const express = require('express');
+
 const app = express();
-const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT ? process.env.PORT : 3001;
+const build = `${__dirname}/build`;
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/build/index.html');
+app.use(express.static(build));
+
+app.get('*', function response(req, res) {
+  res.sendFile(`${__dirname}/index.html`);
 });
 
-app.listen(PORT, error => (
-  error
-    ? console.error(error)
-    : console.info(`Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`)
-));
+app.listen(port, function (error) {
+  if (error) {
+    console.log(error);
+  }
+  console.info('Express is listening on port %s.', port);
+});
