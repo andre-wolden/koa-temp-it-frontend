fc6cc95659a3aa64dbc120f08c7465a703f53a87
André Wolden <andre.wolden@bekk.no>
2018-04-05 09:26:26 +0200

    make it count

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT ? process.env.PORT : 3001;
+const port = process.env.PORT ? process.env.PORT : 8010;
 const build = `${__dirname}/build`;
 
 app.use(express.static(build));
 
 app.get('*', function response(req, res) {
-  res.sendFile(`${__dirname}/index.html`);
+  res.sendFile(`${__dirname}/build/index.html`);
 });
 
 app.listen(port, function (error) {
